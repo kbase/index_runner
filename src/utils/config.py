b4c1c9f2ea1e5054ca5e200ec4b77775575ddf6d
Jay R Bolton <jayrbolton@gmail.com>
2020-02-19 13:58:17 -0800

    Add configuration option to whitelist indices

--- src/utils/config.py
+++ src/utils/config.py
@@ -44,11 +44,13 @@ def get_config():
         raise RuntimeError(f"Invalid global github release url: {github_release_url}")
     gh_token = os.environ.get('GITHUB_TOKEN')
     global_config = _fetch_global_config(config_url, github_release_url, gh_token)
-    skip_indices = set(os.environ.get('SKIP_INDICES', '').split(','))
+    skip_indices = {s for s in os.environ.get('SKIP_INDICES', '').split(',') if s}
+    allow_indices = {s for s in os.environ.get('ALLOW_INDICES', '').split(',') if s}
     return {
         'skip_releng': os.environ.get('SKIP_RELENG'),
         'skip_features': os.environ.get('SKIP_FEATURES'),
         'skip_indices': skip_indices,
+        'allow_indices': allow_indices,
         'global': global_config,
         'github_release_url': github_release_url,
         'github_token': gh_token,
