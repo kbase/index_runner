27fb67318940e7af030590eef29bfdfdfef41dd5
Erik Pearson <eapearson@lbl.gov>
2020-03-19 09:44:11 -0700

    move logging function here from individual modules

--- src/utils/es_utils.py
+++ src/utils/es_utils.py
@@ -1,8 +1,10 @@
 """Elasticsearch API client utilities."""
 import json
 import requests
+import hashlib
 
 from src.utils.config import config
+import src.index_runner.es_indexer as es_indexer
 
 # Initialize configuration data
 _PREFIX = config()['elasticsearch_index_prefix']
@@ -23,3 +25,17 @@ def check_doc_existence(wsid, objid):
     resp_json = resp.json()
     total = resp_json['hits']['total']['value']
     return total > 0
+
+
+def log_err_to_es(msg, err=None):
+    """Log an indexing error in an elasticsearch index."""
+    # The key is a hash of the message data body
+    # The index document is the error string plus the message data itself
+    _id = hashlib.blake2b(json.dumps(msg).encode('utf-8')).hexdigest()
+    es_indexer._write_to_elastic([
+        {
+            'index': config()['error_index_name'],
+            'id': _id,
+            'doc': {'error': str(err), **msg}
+        }
+    ])
