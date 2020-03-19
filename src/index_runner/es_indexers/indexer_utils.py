4ed84bc69f5165356383da3633dc8a9d3a42397f
Erik Pearson <eapearson@lbl.gov>
2020-03-19 10:21:41 -0700

    refactor ws client calls to ws_utils, move handle_id_to_file to ws_utils (since it just wraps ws calls),

--- src/index_runner/es_indexers/indexer_utils.py
+++ src/index_runner/es_indexers/indexer_utils.py
@@ -1,9 +1,6 @@
 import logging
-from kbase_workspace_client import WorkspaceClient
-from kbase_workspace_client.exceptions import WorkspaceResponseError
+import src.utils.ws_utils as ws_utils
 
-from src.utils.config import config
-from src.utils.ws_utils import get_type_pieces
 
 logger = logging.getLogger('IR')
 
@@ -18,14 +15,13 @@ def check_object_deleted(ws_id, obj_id):
     We want to do this because the DELETE event can correspond to more than
     just an object deletion, so we want to make sure the object is deleted
     """
-    ws_client = WorkspaceClient(url=config()['kbase_endpoint'], token=config()['ws_token'])
     try:
-        narr_data_obj_info = ws_client.admin_req("listObjects", {
+        narr_data_obj_info = ws_utils.list_objects({
             'ids': [ws_id]
         })
-    except WorkspaceResponseError as err:
-        logger.error("Workspace response error: ", err.resp_data)
+    except ws_utils.WorkspaceResponseError as err:
         # NOTE: not sure if we want to raise err here, worth thinking about
+        # NOTE: this is already logged by workspace_request
         raise err
     # make sure obj_id is not in list of object ids of workspace (this means its deleted)
     if obj_id not in [obj[0] for obj in narr_data_obj_info]:
@@ -38,8 +34,7 @@ def is_workspace_public(ws_id):
     """
     Check if a workspace is public, returning bool.
     """
-    ws_client = WorkspaceClient(url=config()['kbase_endpoint'], token=config()['ws_token'])
-    ws_info = ws_client.admin_req('getWorkspaceInfo', {'id': ws_id})
+    ws_info = ws_utils.get_workspace_info({'id': ws_id})
     global_read = ws_info[6]
     return global_read != 'n'
 
@@ -50,12 +45,11 @@ def check_workspace_deleted(ws_id):
     we make sure that the workspace is deleted. This is done by making sure we get an excpetion
     with the word 'delete' in the error body.
     """
-    ws_client = WorkspaceClient(url=config()['kbase_endpoint'], token=config()['ws_token'])
     try:
-        ws_client.admin_req("getWorkspaceInfo", {
+        ws_utils.get_workspace_info({
             'id': ws_id
         })
-    except WorkspaceResponseError as err:
+    except ws_utils.WorkspaceResponseError as err:
         if 'delete' in err.resp_text:
             return True
     return False
@@ -67,14 +61,10 @@ def get_shared_users(ws_id):
     Args:
         ws_id - workspace id of requested workspace object
     """
-    ws_client = WorkspaceClient(url=config()['kbase_endpoint'], token=config()['ws_token'])
-    try:
-        obj_perm = ws_client.admin_req("getPermissionsMass", {
-            'workspaces': [{'id': ws_id}]
-        })['perms'][0]
-    except WorkspaceResponseError as err:
-        logger.error("Workspace response error: ", err.resp_data)
-        raise err
+    obj_perm = ws_utils.get_permissions_mass({
+        'workspaces': [{'id': ws_id}]
+    })['perms'][0]
+
     shared_users = []
     for username, user_perms in obj_perm.items():
         if user_perms in ['a', 'r', 'w'] and username != '*':
@@ -89,14 +79,11 @@ def fetch_objects_in_workspace(ws_id, include_narrative=False):
     Args:
         ws_id - a workspace id
     """
-    ws_client = WorkspaceClient(url=config()['kbase_endpoint'], token=config()['ws_token'])
-    try:
-        narr_data_obj_info = ws_client.admin_req("listObjects", {
-            "ids": [ws_id]
-        })
-    except WorkspaceResponseError as err:
-        logger.error("Workspace response error: ", err.resp_data)
-        raise err
+
+    narr_data_obj_info = ws_utils.list_objects({
+        "ids": [ws_id]
+    })
+
     if include_narrative:
         narrative_data = [
             {"obj_id": obj[0], "name": obj[1], "obj_type": obj[2], "ver": obj[4]}
@@ -123,19 +110,18 @@ def _get_tags(ws_info):
         return []
 
 
-def default_fields(obj_data, ws_info, obj_data_v1):
+def default_fields(obj_data, ws_info, obj_info_v1):
     """
     Produce data for fields that are present in any workspace object document on elasticsearch.
     """
     ws_id = obj_data['info'][6]
     obj_id = obj_data['info'][0]
     version = obj_data['info'][4]
-    v1_info = obj_data_v1['info']
     is_public = ws_info[6] == 'r'
     shared_users = get_shared_users(ws_id)
     copy_ref = obj_data.get('copied')
     obj_type = obj_data['info'][2]
-    (type_module, type_name, type_version) = get_type_pieces(obj_type)
+    (type_module, type_name, type_version) = ws_utils.get_type_pieces(obj_type)
     tags = _get_tags(ws_info)
     return {
         "creator": obj_data["creator"],
@@ -143,7 +129,7 @@ def default_fields(obj_data, ws_info, obj_data_v1):
         "obj_name": obj_data['info'][1],
         "shared_users": shared_users,
         "timestamp": obj_data['epoch'],
-        "creation_date": v1_info[3],
+        "creation_date": obj_info_v1[3],
         "is_public": is_public,
         "version": version,
         "obj_id": obj_id,
@@ -153,13 +139,6 @@ def default_fields(obj_data, ws_info, obj_data_v1):
         "obj_type_module": type_module,
         "obj_type_name": type_name
     }
-
-
-def handle_id_to_file(handle_id, dest_path):
-    """given handle id, download associated file from shock."""
-    ws_client = WorkspaceClient(url=config()['kbase_endpoint'], token=config()['ws_token'])
-    shock_id = ws_client.handle_to_shock(handle_id)
-    ws_client.download_shock_file(shock_id, dest_path)
 
 
 def mean(array):
